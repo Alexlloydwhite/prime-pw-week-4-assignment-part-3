72ced2bdf67921e8a76585acc2980214b2924e53
Alex White <alexlloydwhite@gmail.com>
2021-03-18 13:03:52 -0500

    starting to build function for stretch goal #2

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,13 +3,11 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
-
 function addItem( item ){
   console.log( 'in addItem:', item );
   basket.push ( item );
   return true;
 }
-
 console.log( `Basket is: ${basket}` );
 console.log( 'Adding Kumquats:', addItem('Kumquats') );
 console.log( `Basket is now: ${basket}`);
@@ -34,19 +32,16 @@ console.log( `Basket is: ${basket}`);
 listItems( basket );
 
 console.log( '***** Reseting Basket Array *****' );
-
 function empty ( array ){
   console.log( 'in empty:', array);
   array.length = 0;
   return array;
 }
-
 console.log( `Basket is: ${basket}` );
 console.log( 'Emptying array:', empty(basket) );
 console.log( `Basket is now: ${basket}` );
 
 console.log( '***** Stretch Goals: Using Functions in Other Functions!' );
-
 const maxItems = 5;
 function isFull( array ){
   console.log( 'in isFull:', array );
@@ -56,4 +51,26 @@ function isFull( array ){
     return true;
   }
 }
-console.log( isFull(basket) );
+console.log( 'The basket is empty so this should return false:', isFull(basket) );
+
+console.log( '***** Stretch Goals: Using Array Built-In Functions!' );
+basket.push('apple', 'oranges', 'lemons', 'cheese');
+console.log(basket);
+
+function removeItem ( item ){
+  console.log( 'in removeItem:',item );
+  for (i=0;i<basket.length;i++){
+      if ( basket.indexOf(item, 0) === item ){
+        basket.splice(item);
+        return item;
+      } else {
+        return null;
+      }
+    }
+}
+console.log( removeItem( 'apple' ) );
+console.log( basket );
+
+// ArrayindexOf(searchElement, fromIndex)
+// 1. The searchElement argument is the element that you want to find in the array.
+// 2. The fromIndex is an array index at which the function starts the search.
